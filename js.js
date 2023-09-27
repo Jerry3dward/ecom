be25bbc231a8127d9930e11a9c57f352c03bf8ab
jerry3dward <badiashfak42@gmail.com>
2023-09-27 11:09:53 +0530

    adding javascript for test

---
+++ js.js
@@ -0,0 +1,15 @@
+let a = 10;
+let b = 7 * a;
+console.log(9*a)
+console.log(b)
+
+const max = 57;
+const actual = max - 13;
+const per = actual/max;
+
+let x = "1";
+let y = "2";
+console.log(x+y)
+
+let index = "hello this is first javascript";
+alert(index)
